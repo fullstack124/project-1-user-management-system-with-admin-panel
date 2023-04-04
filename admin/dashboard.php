7612464401e4e686344a5f3d0af79221458a2e99
hilalahmad32 <programmerhero6@gmail.com>
2023-04-05 02:59:33 +0500

    layout-setup

---
+++ admin/dashboard.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap"
+      rel="stylesheet"
+    />
+
+    <link rel="stylesheet" href="../fonts/icomoon/style.css" />
+
+    <link rel="stylesheet" href="../css/owl.carousel.min.css" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css" />
+
+    <!-- Style -->
+    <link rel="stylesheet" href="../css/style.css" />
+
+    <title>Login #6</title>
+  </head>
+  <body>
+        <div class="container" style="margin-top: 40px;">
+          <div class="row d-flex justify-content-center">
+            <div class="col-12">
+                <div class="table-responsive">
+                    <h3 class="my-4">Users</h3>
+                    <a href="" class="nav-link">Logout</a>
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Delete</th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+          </div>
+        </div>
+
+    <script src="../js/jquery-3.3.1.min.js"></script>
+    <script src="../js/popper.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/main.js"></script>
+  </body>
+</html>
---
+++ home.php
@@ -0,0 +1,32 @@
+
+    <div class="d-lg-flex half">
+      <div
+        class="bg order-1 order-md-2"
+        style="background-image: url('images/bg_1.jpg')"
+      ></div>
+      <div class="contents order-2 order-md-1">
+        <div class="container">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-md-7">
+              <form action="#" method="post">
+                <div class="social-login">
+                    <h1 class="text-center">Hilal ahmad</h1>
+                  <a
+                    href="#"
+                    class="facebook btn d-flex justify-content-center align-items-center"
+                  > Logout
+                  </a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
